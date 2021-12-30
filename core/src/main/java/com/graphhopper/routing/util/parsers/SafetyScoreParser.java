6031bd15ad319605f14def50391819c19c7c298b
Denis Koljada <denis.koljada@campus.tu-berlin.de>
2021-12-30 19:32:08 +0100

    started work on safety score tag parser

---
+++ core/src/main/java/com/graphhopper/routing/util/parsers/SafetyScoreParser.java
@@ -0,0 +1,31 @@
+package com.graphhopper.routing.util.parsers;
+
+import com.graphhopper.reader.ReaderWay;
+import com.graphhopper.routing.ev.EncodedValue;
+import com.graphhopper.routing.ev.EncodedValueLookup;
+import com.graphhopper.routing.ev.IntEncodedValue;
+import com.graphhopper.routing.ev.SafetyScore;
+import com.graphhopper.storage.IntsRef;
+
+import java.util.List;
+
+/**
+ * SimRa safety score parser for safety score values
+ */
+public class SafetyScoreParser implements TagParser {
+    private final IntEncodedValue safetyScoreEnc;
+
+    public SafetyScoreParser() {
+        this.safetyScoreEnc = SafetyScore.create();
+    }
+
+    @Override
+    public void createEncodedValues(EncodedValueLookup lookup, List<EncodedValue> registerNewEncodedValue) {
+        registerNewEncodedValue.add(safetyScoreEnc);
+    }
+
+    @Override
+    public IntsRef handleWayTags(IntsRef edgeFlags, ReaderWay way, IntsRef relationFlags) {
+        return null;
+    }
+}
