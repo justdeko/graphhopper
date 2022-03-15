3be49a27a3ee075835935212cde80e2c739aa08f
Denis Koljada <denis.koljada@campus.tu-berlin.de>
2022-03-15 11:13:55 +0100

    fixed filename and .csv encoding

--- core/src/main/java/com/graphhopper/routing/util/parsers/ScoreEntry.java
+++ core/src/main/java/com/graphhopper/routing/util/parsers/ScoreEntry.java
@@ -1,9 +1,11 @@
 package com.graphhopper.routing.util.parsers;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({"way_id", "score"})
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ScoreEntry {
     @JsonProperty("way_id")
     public long id;
